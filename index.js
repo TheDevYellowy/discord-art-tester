40d9d6be78836a2ba43ffc91fea464cee8a1b520
TheDevYellowy <yellowycorn6963@gmail.com>
2024-10-25 16:27:29 -0500

    write a quick bot to detect ai generated images using sightengine.com

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Client, IntentsBitField, Partials } = require("discord.js");
+const client = new Client({ intents: [ IntentsBitField.Flags.GuildMessages ], partials: [ Partials.Message ]});
+const axios = require("axios");
+require('dotenv').config();
+
+client.on("messageCreate", (message) => {
+  if(message.member.permissions.has("ManageMessages")) return;
+  if(message.attachments.size < 1) return;
+  for(let i = 0; i < message.attachments.size; i++) {
+    const url = message.attachments.at(i).url;
+    axios.get(`https://api.sightengine.com/1.0/check.json`, {
+      params: {
+        "url": url,
+        "models": "genai",
+        "api_user": process.env.api_user,
+        "api_secret": process.env.api_secret
+      }
+    }).then(({ data }) => {
+      if(data.type.ai_generated > 0.6) message.delete();
+    });
+  }
+});
+
+client.login(process.env.discord_token)
