8a9e26d39bba2e040cea6d001285a788f395b611
TheDevYellowy <yellowycorn6963@gmail.com>
2025-01-16 02:19:38 -0600

    add catch to axios get

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
 const { Client, IntentsBitField, Partials } = require("discord.js");
-const client = new Client({ intents: [ IntentsBitField.Flags.GuildMessages ], partials: [ Partials.Message ]});
+const client = new Client({
+  intents: [IntentsBitField.Flags.GuildMessages],
+  partials: [Partials.Message],
+});
 const axios = require("axios");
-require('dotenv').config();
+require("dotenv").config();
 
 client.on("messageCreate", (message) => {
-  if(message.member.permissions.has("ManageMessages")) return;
-  if(message.attachments.size < 1) return;
-  for(let i = 0; i < message.attachments.size; i++) {
+  if (message.member.permissions.has("ManageMessages")) return;
+  if (message.attachments.size < 1) return;
+  for (let i = 0; i < message.attachments.size; i++) {
     const url = message.attachments.at(i).url;
-    axios.get(`https://api.sightengine.com/1.0/check.json`, {
-      params: {
-        "url": url,
-        "models": "genai",
-        "api_user": process.env.api_user,
-        "api_secret": process.env.api_secret
-      }
-    }).then(({ data }) => {
-      if(data.type.ai_generated > 0.6) {
-        console.log(`Art sent by ${message.author.displayName} is AI`);
-        message.delete()
-      } else console.log(`Art sent by ${message.author.displayName} is not AI`);
-    });
+    axios
+      .get(`https://api.sightengine.com/1.0/check.json`, {
+        params: {
+          url: url,
+          models: "genai",
+          api_user: process.env.api_user,
+          api_secret: process.env.api_secret,
+        },
+      })
+      .then(({ data }) => {
+        console.log({ data });
+        if (data.type.ai_generated > 0.6) {
+          console.log(`Art sent by ${message.author.displayName} is AI`);
+          message.delete();
+        } else
+          console.log(`Art sent by ${message.author.displayName} is not AI`);
+      })
+      .catch((error) => {
+        if (error.response) console.log(error.response.data);
+        else console.log(error.message);
+      });
   }
 });
 
-client.once("ready", () => { console.log(`${client.user.displayName} is online`) });
+client.once("ready", () => {
+  console.log(`${client.user.displayName} is online`);
+});
 
 client.login(process.env.discord_token);
